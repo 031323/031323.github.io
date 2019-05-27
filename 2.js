dd86de5b8ab358330e0d480fe3828bb7435cf11a
142434 <utkars.rajput@gmail.com>
2019-05-27 18:26:29 +0530

    115

--- 2.js
+++ 2.js
@@ -1,49 +1,9 @@
-var al,bl,h,m,observer,cursor;
-var d=0.02;
-// Select the node that will be observed for mutations
-var targetNode = document.getElementById('al');
-
-// Options for the observer (which mutations to observe)
-var config = { attributes: true, childList: true, subtree: true };
-
-// Callback function to execute when mutations are observed
-var callback = function(mutationsList, observer) {
-    for(var mutation of mutationsList) {
-        if (mutation.type == 'childList') {
-            console.log('A child node has been added or removed.');
-        }
-        else if (mutation.type == 'attributes') {
-            //console.log('The ' + mutation.attributeName + ' attribute was modified.');
-        }
-    }
-    if(al.offsetHeight==h)
-	{
-		m+=d;
-		al.style.marginLeft=m.toString()+"em";
-		al.style.marginRight=m.toString()+"em";
-	}
-	else
-	{
-		m-=d;
-		al.style.marginLeft=m.toString()+"em";
-		al.style.marginRight=m.toString()+"em";
-		al.style.color="black";
-		observer.disconnect();
-	}
-};
-
-// Create an observer instance linked to the callback function
-
-
-// Start observing the target node for configured mutations
+var bl,cursor;
 
 function flow()
 {
 	console.log("laoded");
-	al=document.getElementById("al");
 	bl=document.getElementById("bl");
-	h=al.offsetHeight;
-	m=parseFloat(al.style.marginRight);
 	
 	
 	cursor=document.getElementById("cursor");	
@@ -58,12 +18,5 @@ function flow()
 	var m2l=Math.floor(ml/2);
 	bl.style.marginLeft=(m2l).toString()+"px";
 	bl.style.marginRight=(ml-m2l).toString()+"px";
-	//cursor.style.color="white";
-	al.style.color="black";
-	
-	/*observer = new MutationObserver(callback);
-	observer.observe(targetNode, config);
-	m+=d;
-	al.style.marginLeft=m.toString()+"em";
-	al.style.marginRight=m.toString()+"em";*/
+	bl.style.color="black";
 }
