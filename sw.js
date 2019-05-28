3b859030ca6c582cd382b6eea5ca5077b370795c
142434 <utkars.rajput@gmail.com>
2019-05-28 07:41:38 +0530

    117

--- sw.js
+++ sw.js
@@ -18,4 +18,17 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
   );
+  event.waitUntil(
+    update(event.request)
 });
+
+
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  });
+}
