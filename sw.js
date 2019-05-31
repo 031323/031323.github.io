04ad9ca73abe6c27e7a73458c1757373590c6ad8
142434 <utkars.rajput@gmail.com>
2019-05-31 18:19:51 +0530

    132

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v201905301814').then(function(cache) {
+    caches.open('v201905311819').then(function(cache) {
       return cache.addAll([
         '/',
         '/नर्मदादर्शनकथा',
@@ -17,7 +17,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-  var cacheKeeplist = ['v201905301814'];
+  var cacheKeeplist = ['v201905311819'];
 
   event.waitUntil(
     caches.keys().then(function(keyList) {
