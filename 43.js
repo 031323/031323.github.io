0fca6d49a06a971f2519e69475752e2e42c13447
142434 <utkars.rajput@gmail.com>
2019-08-07 17:28:46 +0530

    200

--- 43.js
+++ 43.js
@@ -11,28 +11,63 @@ function getTextWidth(text, font) {
     return metrics.width;
 }
 
-function f43(x1,x2,x3,d)
+function f43(x1,x2,x3,d,G=false)
 {
+	var w1=new Array();
+	var i;
 	x1=x1.concat(["","",""]);
+	if(G)
+	{
+		for(i=0;i<x1.length;i++)
+			w1.push(getTextWidth(x1[i],css(d,'font-weight')+" "+css(d,'font-size')+" "+css(d,'font-family')));
+	}
+	
 	var n=x1.length/4;
 	if(n==1)return x1[0];
-	var i,r="",R="";
+	var r="",R="",rw=0;
 	var x4=x2;
 	if(x3!=0)
 	{
-		if(getTextWidth(x1[0]+x1[1],css(d,'font-weight')+" "+css(d,'font-size')+" "+css(d,'font-family'))>x2-x3)R+="<br>";
-		else x4=x2-x3;
-	}
-	r+=x1[0];
-	for(i=1;i<n;i++)
-	{
-		if(getTextWidth(r+x1[i*4-2]+x1[i*4]+x1[i*4+1],css(d,'font-weight')+" "+css(d,'font-size')+" "+css(d,'font-family'))<=x4)
-			r+=x1[i*4-2]+x1[i*4];
+		if(G)
+		{
+			if(w1[0]+w1[1]>x2-x3)R+="<br>";
+			else x4=x2-x3;
+		}
 		else
 		{
-			x4=x2;
-			R+=r+x1[i*4-3]+"<br>";
-			r=x1[i*4-1]+x1[i*4];
+			if(getTextWidth(x1[0]+x1[1],css(d,'font-weight')+" "+css(d,'font-size')+" "+css(d,'font-family'))>x2-x3)R+="<br>";
+			else x4=x2-x3;
+		}
+	}
+	r+=x1[0];
+	if(G)rw+=w1[0];
+	for(i=1;i<n;i++)
+	{
+		if(G)
+		{
+			if(rw+w1[i*4-2]+w1[i*4]+w1[i*4+1]<=x4)
+			{
+				r+=x1[i*4-2]+x1[i*4];
+				rw+=w1[i*4-2]+w1[i*4];
+			}
+			else
+			{
+				x4=x2;
+				R+=r+x1[i*4-3]+"<br>";
+				r=x1[i*4-1]+x1[i*4];
+				rw=w1[i*4-1]+w1[i*4];
+			}
+		}
+		else
+		{
+			if(getTextWidth(r+x1[i*4-2]+x1[i*4]+x1[i*4+1],css(d,'font-weight')+" "+css(d,'font-size')+" "+css(d,'font-family'))<=x4)
+				r+=x1[i*4-2]+x1[i*4];
+			else
+			{
+				x4=x2;
+				R+=r+x1[i*4-3]+"<br>";
+				r=x1[i*4-1]+x1[i*4];
+			}
 		}
 	}
 	R+=r+x1[i*4-3];
